169484bfe1a70af886d48c751347ecb32b93ed7c
unknown <anujreoti7398@gmail.com>
2024-07-24 10:28:57 +0530

    new update add two number

---
+++ loop.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class loop {
+    public static void main(String[] args) {
+        //for loop
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i);
+        }
+        System.out.println();
+
+
+//while loop
+        int j = 10;
+        while (j > 0) {
+            System.out.print(j);
+            j--;
+        }
+        System.out.println();
+
+
+
+        //do-while
+        int i=1;
+        do {
+            System.out.print(i);
+            i++;
+        }while(i<=10);
+
+    }
+
+
+}
