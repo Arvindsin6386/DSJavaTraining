7fc3e7fde30f301617525e4ed2edab44404618db
unknown <anujreoti7398@gmail.com>
2024-07-24 17:05:57 +0530

    arry in java

---
+++ ArrayDemo.java
@@ -0,0 +1,20 @@
+public class ArrayDemo {
+    public static void main(String[] args) {
+        //Declare an array to store names
+        String[]names=new String[]{"anuj","deepika","riya","jones"};
+       // names[0]="anuj";
+      //  names[1]="jones";
+       // names[2]="aditya";
+       // names[3]="riya";
+     //   names[4]="deepika";
+        for(int i=0;i<names.length;i++){
+            System.out.print(names[i]);
+        }
+names[2]="rahul";
+        for(int i=0;i<names.length;i++){
+            System.out.println(names[i]);
+        }
+//        System.out.println(names[i]);
+    }
+    //
+}
